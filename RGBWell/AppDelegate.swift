fb29d4c065688343b5da16ee05a20e57d136ec01
Rubén Jiménez <ruben@21ninjas.es>
2015-11-09 23:13:55 +0000

    Initial Commit

---
+++ RGBWell/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RGBWell
+//
+//  Created by Rubén Jiménez on 9/11/15.
+//  Copyright © 2015 21ninjas, SL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
